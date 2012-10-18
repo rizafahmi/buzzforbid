8028363857f923288d96bb43ce53239aeed24557
Riza Fahmi <rizafahmi@gmail.com>
2012-10-18 15:58:19 +0700

    alter hotel relation with manager. ERROor

---
+++ hotel/migrations/0011_auto__del_hotelmanageruser__del_hotelsupervisoruser__del_hoteluser__de.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting model 'HotelManagerUser'
+        db.delete_table('hotel_hotelmanageruser')
+
+        # Deleting model 'HotelSupervisorUser'
+        db.delete_table('hotel_hotelsupervisoruser')
+
+        # Deleting model 'HotelUser'
+        db.delete_table('hotel_hoteluser')
+
+        # Deleting field 'Hotel.user'
+        db.delete_column('hotel_hotel', 'user_id')
+
+        # Adding field 'Hotel.receptionist'
+        db.add_column('hotel_hotel', 'receptionist',
+                      self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='receptionist_user', null=True, to=orm['user_level.Receptionist']),
+                      keep_default=False)
+
+
+        # Changing field 'Hotel.supervisor'
+        db.alter_column('hotel_hotel', 'supervisor_id', self.gf('django.db.models.fields.related.ForeignKey')(null=True, to=orm['user_level.Supervisor']))
+
+        # Changing field 'Hotel.manager'
+        db.alter_column('hotel_hotel', 'manager_id', self.gf('django.db.models.fields.related.ForeignKey')(null=True, to=orm['user_level.Manager']))
+    def backwards(self, orm):
+        # Adding model 'HotelManagerUser'
+        db.create_table('hotel_hotelmanageruser', (
+            ('phone_number', self.gf('django.db.models.fields.CharField')(max_length=50, null=True, blank=True)),
+            ('user', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['auth.User'], unique=True)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, unique=True)),
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        ))
+        db.send_create_signal('hotel', ['HotelManagerUser'])
+
+        # Adding model 'HotelSupervisorUser'
+        db.create_table('hotel_hotelsupervisoruser', (
+            ('phone_number', self.gf('django.db.models.fields.CharField')(max_length=50, null=True, blank=True)),
+            ('user', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['auth.User'], unique=True)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, unique=True)),
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        ))
+        db.send_create_signal('hotel', ['HotelSupervisorUser'])
+
+        # Adding model 'HotelUser'
+        db.create_table('hotel_hoteluser', (
+            ('phone_number', self.gf('django.db.models.fields.CharField')(max_length=50, null=True, blank=True)),
+            ('user', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['auth.User'], unique=True)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, unique=True)),
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        ))
+        db.send_create_signal('hotel', ['HotelUser'])
+
+        # Adding field 'Hotel.user'
+        db.add_column('hotel_hotel', 'user',
+                      self.gf('django.db.models.fields.related.ForeignKey')(related_name='user_user', null=True, to=orm['hotel.HotelUser'], blank=True),
+                      keep_default=False)
+
+        # Deleting field 'Hotel.receptionist'
+        db.delete_column('hotel_hotel', 'receptionist_id')
+
+
+        # Changing field 'Hotel.supervisor'
+        db.alter_column('hotel_hotel', 'supervisor_id', self.gf('django.db.models.fields.related.ForeignKey')(null=True, to=orm['hotel.HotelSupervisorUser']))
+
+        # Changing field 'Hotel.manager'
+        db.alter_column('hotel_hotel', 'manager_id', self.gf('django.db.models.fields.related.ForeignKey')(null=True, to=orm['hotel.HotelManagerUser']))
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'geographic_info.city': {
+            'Meta': {'object_name': 'City'},
+            'city': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'geographic_info.province': {
+            'Meta': {'object_name': 'Province'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'})
+        },
+        'geographic_info.region': {
+            'Meta': {'object_name': 'Region'},
+            'city': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['geographic_info.City']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'region_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        'hotel.facility': {
+            'Meta': {'object_name': 'Facility'},
+            'facility': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'hotel.hotel': {
+            'Meta': {'object_name': 'Hotel'},
+            'address1': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'address2': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'city': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'hotel_city'", 'to': "orm['geographic_info.City']"}),
+            'country': ('django.db.models.fields.CharField', [], {'default': "'Indonesia'", 'max_length': '35'}),
+            'cs_email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'facilities': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'hotel_facility'", 'symmetrical': 'False', 'to': "orm['hotel.Facility']"}),
+            'fax_number': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'manager': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'manager_user'", 'null': 'True', 'to': "orm['user_level.Manager']"}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'phone_number': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'province': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'hotel_province'", 'null': 'True', 'to': "orm['geographic_info.Province']"}),
+            'rating': ('django.db.models.fields.IntegerField', [], {'default': '3'}),
+            'receptionist': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'receptionist_user'", 'null': 'True', 'to': "orm['user_level.Receptionist']"}),
+            'region': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'hotel_region'", 'null': 'True', 'to': "orm['geographic_info.Region']"}),
+            'room_type': ('django.db.models.fields.CharField', [], {'max_length': '3'}),
+            'supervisor': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'supervisor_user'", 'null': 'True', 'to': "orm['user_level.Supervisor']"}),
+            'zipcode': ('django.db.models.fields.CharField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'})
+        },
+        'user_level.manager': {
+            'Meta': {'object_name': 'Manager'},
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'phone_number': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
+        },
+        'user_level.receptionist': {
+            'Meta': {'object_name': 'Receptionist'},
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'phone_number': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
+        },
+        'user_level.supervisor': {
+            'Meta': {'object_name': 'Supervisor'},
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'phone_number': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
+        }
+    }
+
+    complete_apps = ['hotel']
